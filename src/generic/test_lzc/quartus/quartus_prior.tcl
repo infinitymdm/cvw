0561cc86ee95bc38f7b724da86d5253aa89b2a5a
Marcus Mellor <infinitymdm@gmail.com>
2025-11-19 13:55:48 -0600

    lzc: Add quartus outputs

--- src/generic/test_lzc/quartus/quartus_prior.tcl
+++ src/generic/test_lzc/quartus/quartus_prior.tcl
@@ -1,12 +1,16 @@
 package require ::quartus::project
 package require ::quartus::flow
+package require ::quartus::sta
 
 set PROJECT_NAME "lzc_prior"
 set PWD [pwd]
 
 project_new $PROJECT_NAME -overwrite
-set_global_assignment -name PROJECT_OUTPUT_DIRECTORY "$PWD/quartus/output_files"
+set_global_assignment -name PROJECT_OUTPUT_DIRECTORY "$PWD/quartus/outputs_prior"
 set_global_assignment -name TOP_LEVEL_ENTITY "lzc_prior"
 set_global_assignment -name SYSTEMVERILOG_FILE "$PWD/lzc_prior.sv"
-execute_flow -analysis_and_elaboration
+if {[catch {execute_flow -analysis_and_elaboration} result]} {
+    # We expect an error here
+    project_close
+}
 project_close
--- src/generic/test_lzc/quartus/quartus_recursive_opt.tcl
+++ src/generic/test_lzc/quartus/quartus_recursive_opt.tcl
@@ -1,12 +1,17 @@
 package require ::quartus::project
 package require ::quartus::flow
+package require ::quartus::sta
 
 set PROJECT_NAME "lzc_recursive_opt"
 set PWD [pwd]
 
 project_new $PROJECT_NAME -overwrite
-set_global_assignment -name PROJECT_OUTPUT_DIRECTORY "$PWD/quartus/output_files"
+set_global_assignment -name PROJECT_OUTPUT_DIRECTORY "$PWD/quartus/outputs_recursive_opt"
 set_global_assignment -name TOP_LEVEL_ENTITY "lzc"
 set_global_assignment -name SYSTEMVERILOG_FILE "$PWD/../lzc.sv"
 execute_flow -analysis_and_elaboration
+execute_flow -compile
+create_timing_netlist -post_map
+report_timing -setup -npaths 100 -detail full_path
+check_timing
 project_close
